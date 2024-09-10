f79fba6c98197ef415cd3f2b84dd811eed6b0756
Leander Jedamus <ljedamus@web.de>
2024-09-10 11:04:41 +0200

    added url.txt and description.txt

--- create_project.sh
+++ create_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Freitag, 16. Februar 2024 19:38 (C) 2024 von Leander Jedamus
-# modifiziert Dienstag, 10. September 2024 09:16 von Leander Jedamus
+# modifiziert Dienstag, 10. September 2024 11:02 von Leander Jedamus
 # modifiziert Freitag, 16. August 2024 09:02 von Leander Jedamus
 # modifiziert Donnerstag, 15. August 2024 16:53 von Leander Jedamus
 # modifiziert Montag, 12. August 2024 16:24 von Leander Jedamus
@@ -32,14 +32,17 @@ author_email_filename="author_email.txt"
 license_filename="license.txt"
 maintainer_filename="maintainer.txt"
 maintainer_email_filename="maintainer_email.txt"
+url_filename="url.txt"
+description_filename="description.txt"
 years_filename="years.txt"
 
-for file in $project_filename $author_filename $author_email_filename $license_filename $maintainer_filename $maintainer_email_filename $years_filename; do
+for file in $project_filename $author_filename $author_email_filename $license_filename $maintainer_filename $maintainer_email_filename \
+            $url_filename $description_filename $years_filename; do
   if [ ! -f $file ]; then
     echo "$file not found."
     exit 1
-#  else
-#    echo "$file found."
+  else
+    echo "$file found."
   fi
 done
 
@@ -49,6 +52,8 @@ author_email=$(cat $author_email_filename)
 license=$(cat $license_filename)
 maintainer=$(cat $maintainer_filename)
 maintainer_email=$(cat $maintainer_email_filename)
+url=$(cat $url_filename)
+description=$(cat $description_filename)
 years=$(cat $years_filename)
 
 # FILENAME=$(echo $filename | sed -e 's/\./_/' | tr a-z A-Z)
@@ -71,6 +76,8 @@ cat <<EOF > $filename
 #define LICENSE "$license"
 #define MAINTAINER "$maintainer"
 #define MAINTAINER_EMAIL "$maintainer_email"
+#define URL "$url"
+#define DESCRIPTION "$description"
 #define YEARS "$years"
 
 #endif /* $FILENAME */
