c4392fb9d8e56ce7b88490a3c29f3dd93cb7ab18
Leander Jedamus <ljedamus@web.de>
2024-10-01 00:50:15 +0200

    added .texinfo.in, .txinfo.in and .texi.in support

--- create_version.sh
+++ create_version.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 # erzeugt Freitag, 16. Februar 2024 19:38 (C) 2024 von Leander Jedamus
+# modifiziert Dienstag, 01. Oktober 2024 00:48 von Leander Jedamus
+# modifiziert Montag, 30. September 2024 23:26 von Leander Jedamus
 # modifiziert Dienstag, 10. September 2024 09:31 von Leander Jedamus
 # modifiziert Freitag, 16. August 2024 09:02 von Leander Jedamus
 # modifiziert Donnerstag, 15. August 2024 16:53 von Leander Jedamus
@@ -18,6 +20,8 @@ if [ -z $1 ]; then
   exit 1
 fi
 
+the_version=the_version
+
 filename=$1
 if [ -z $2 ]; then
   base_filename=$(basename $filename)
@@ -26,16 +30,20 @@ else
 fi
 
 version_filename=version.txt
+updated_filename=updated.txt
 author_filename=author.txt
 
-for file in $version_filename $author_filename; do
+for file in $version_filename $updated_filename $author_filename; do
   if [ ! -f $file ]; then
     echo "$file not found."
     exit 1
+#  else
+#    echo "$file found."
   fi
 done
 
 author=$(cat $author_filename)
+updated=$(cat $updated_filename)
 version=$(cat $version_filename)
 
 # FILENAME=$(echo $filename | sed -e 's/\./_/' | tr a-z A-Z)
@@ -58,12 +66,23 @@ cat <<EOF > $filename
 #define $FILENAME 1
 
 #define VERSION "$version"
+#define UPDATED "$updated"
 
 #endif /* $FILENAME */
 
 /* vim:set cindent ai sw=2 */
 
 EOF
+
+for file in $the_version.texinfo $the_version.txinfo $the_version.texi; do
+  if [ -f $file.in ]; then
+    # echo "$file for input found."
+    sed \
+      -e "s/[@]VERSION[@]/$version/g" \
+      -e "s/[@]UPDATED[@]/$updated/g" \
+      < $file.in > $file
+  fi
+done
 
 # vim:ai sw=2
 
